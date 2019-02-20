85c99eddc01f879aaa3c1936afbca152c4bc1e4f
Unknown <sarah4594@gmail.com>
2019-02-19 21:47:13 -0500

    Create first component!

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { STATUS_CODES } from 'http'
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello from StorePicker</p>
+  }
+}
+
+export default StorePicker
