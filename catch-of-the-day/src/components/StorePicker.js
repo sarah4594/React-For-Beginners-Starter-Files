9bc5412a0e161f535f4e2a8e67b9270e1e9382c0
Unknown <sarah4594@gmail.com>
2019-02-25 20:48:19 -0500

    Add submit handler

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef()
+
+  goToStore = event => {
+    event.preventDefault()
+    console.log(this)
+  }
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
