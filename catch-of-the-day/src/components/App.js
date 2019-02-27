4c84eb445ce305c85a2f2ff1cb5268feded124bd
Unknown <sarah4594@gmail.com>
2019-02-26 22:22:04 -0500

    Create way to add fish to form using states

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = fish => {
+    // 1. take a copy of existing state
+    const fishes = { ...this.state.fishes }
+    // 2. add out new fish to fishes variable (create unique key to keep everything separate)
+    fishes[`fish${Date.now()}`] = fish
+    // 2. set new fishes object to state
+    this.setState({
+      fishes //(fishes: fishes)
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
